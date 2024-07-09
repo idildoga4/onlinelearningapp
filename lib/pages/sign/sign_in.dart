8902744eb0fc27a52ea2d7658a069853a237ca70
idildoga4 <111755801+idildoga4@users.noreply.github.com>
2024-07-09 16:23:03 +0300

    push

--- lib/pages/sign/sign_in.dart
+++ lib/pages/sign/sign_in.dart
@@ -1,35 +1,60 @@
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ulearning/common/entities/colors.dart';
 
-class SignIn extends StatefulWidget{
-  const SignIn({Key? key}):super(key:key);
-  @override 
-  State<SignIn> createState()=> _SignState();
- }
+import 'widgets/sign_in_widget.dart';
 
- class _SignState extends State<SignIn> {
-  @override 
+class SignIn extends StatefulWidget {
+  const SignIn({super.key});
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white, 
-      child:SafeArea(
+      color: AppColors.primaryBackground,
+      child: SafeArea(
         child: Scaffold(
-          appBar: AppBar(
-
-            bottom: PreferredSize(preferredSize: const Size.fromHeight(1.0),
-            child:Container(
-              color:Colors.grey.withOpacity(0.5),
-              height: 1.0,
-            )),
-            title: Text("Log In",
-            style: TextStyle(
-              color:Colors.black,
-              fontSize:16.sp,
-              fontWeight: FontWeight.normal,
-            ))
-          )
-        )
-      )
+          backgroundColor: AppColors.primaryBackground,
+          appBar: buildAppBar(),
+          body: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                buildThirdPartyLogin(context),
+                Center(child: reusableText("Or use your email account to login")),
+                Container(
+                  margin: EdgeInsets.only(top: 40.h),
+                  padding: EdgeInsets.only(left: 25.w,right: 25.w),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      reusableText("Email"),
+                      SizedBox(height: 5.h,),
+                      buildTextField("Enter your email address", "email","user"),
+                      reusableText("Password"),
+                      SizedBox(height: 5.h,),
+                      buildTextField("Enter your password", "password","lock"),
+                      SizedBox(height: 20.h,),
+                      forgotPassword(),
+                      buildLogInAndRegButton("Log In", "login"),
+                      buildLogInAndRegButton("Register", "register"),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+    
+        ),
+      ),
     );
   }
- }
+}
